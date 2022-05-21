39d19161f65369538c95c22c2e3ea24eaace888f
MikuSenpai <105959938+MikuSenpai@users.noreply.github.com>
2022-05-21 11:47:34 +0700

    Update creator2.js

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -3,11 +3,11 @@ const PhoneNumber = require('awesome-phonenumber')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   await conn.sendMessage(m.chat, {
     contacts: [{
-    "displayName": "AxellXyz",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:AxellXyz\nitem1.TEL;waid=6288289338073:6288289338073\nitem1.X-ABLabel:📍 Creator ShinnBOT\nitem2.EMAIL;type=INTERNET:axellxyzz@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://bit.ly/3kXpKwL/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Provinsi | DKI JAKARTA\nitem5.X-ABLabel:──────[ AxellXyz ]──────\nEND:VCARD"
+    "displayName": "Indra",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN: Indra\nitem1.TEL;waid=6282228302341:6282228302341\nitem1.X-ABLabel:📍 Creator MikuBOT\nitem2.EMAIL;type=INTERNET:axellxyzz@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://bit.ly/3kXpKwL/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Provinsi | DKI JAKARTA\nitem5.X-ABLabel:──────[ Indra ]──────\nEND:VCARD"
   }, {
-    "displayName": "TataXyz",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:TataXyz\nitem1.TEL;waid=6282157312489:6282157312489\nitem1.X-ABLabel:📍 Couple AxellXyz\nitem2.EMAIL;type=INTERNET:axellxyzz@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://bit.ly/3kXpKwL/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Provinsi | KALIMANTAN\nitem5.X-ABLabel:────[ TataXyz ]────\nEND:VCARD"
+    "displayName": "Indra2",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Indra2\nitem1.TEL;waid=6282132536871:6282132536871\nitem1.X-ABLabel:📍 Bot Miku\nitem2.EMAIL;type=INTERNET:axellxyzz@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://bit.ly/3kXpKwL/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Provinsi | KALIMANTAN\nitem5.X-ABLabel:────[ Indra2]────\nEND:VCARD"
     }]
   }, MessageType.contactsArray, { quoted: m })
 }
